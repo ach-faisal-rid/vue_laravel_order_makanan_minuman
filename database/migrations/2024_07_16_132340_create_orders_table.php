d523957b9bacf0e38ad8ac481ae6f7e6e0cc5662
ach-faisal-rid <faizal.a.ridhani@gmail.com>
2024-07-16 20:49:36 +0700

    initialize project vue-laravel

---
+++ database/migrations/2024_07_16_132340_create_orders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('customer_name', 100);
+            $table->string('table_no', 2);
+            $table->string('status', 100);
+            $table->integer('total')->unsigned();
+            $table->unsignedBigInteger('waitress_id');
+            $table->unsignedBigInteger('chasier_id');
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('waitress_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('chasier_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
