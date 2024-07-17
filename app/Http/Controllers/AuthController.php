b8c64df567323054b2fa88c17669742f61ee2474
ach-faisal-rid <faizal.a.ridhani@gmail.com>
2024-07-17 21:01:24 +0700

    make login, seeder user, role

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    // proses login
+    public function login(Request $request) {
+        return 'oke deh beres';
+    }
+}
