88eb32cee3ca9a9c1a3939a2eb06227fbe544d24
ach-faisal-rid <faizal.a.ridhani@gmail.com>
2024-07-17 21:27:30 +0700

    make login and add expiration

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,11 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     // proses login
     public function login(Request $request) {
-        return 'oke deh beres';
+        $validated = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $user = User::where('email', $validated['email'])->first();
+
+        if (! $user || ! Hash::check($validated['password'], $user->password)) {
+            return response()->json(['message' => 'Credentials are incorrect.'], 401);
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'success' => true,
+            'user' => [
+                'id' => $user->id,
+                'name' => $user->name,
+                'email' => $user->email,
+                'level' => $user->role_id,
+                'tgl_buat' =>date('Y-m-d H:i:s', strtotime($user->timestamps)),
+            ],
+            'message' => 'login berhasil',
+            'token' => $token,
+        ], 200);
+
     }
 }
