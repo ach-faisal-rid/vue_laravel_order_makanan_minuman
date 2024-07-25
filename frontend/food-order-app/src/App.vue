e24e09208df68d9517fad108d2eba6314e4ce532
ach-faisal-rid <faizal.a.ridhani@gmail.com>
2024-07-25 21:53:18 +0700

    install vue js + add login and header

---
+++ frontend/food-order-app/src/App.vue
@@ -0,0 +1,64 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+    <header>
+        <nav class="bg-gray-100 p-4">
+            <div class="container mx-auto flex flex-wrap items-center justify-between">
+                <div class="flex justify-between w-full lg:w-auto">
+                    <RouterLink to="/" class="text-xl font-semibold">Navbar</RouterLink>
+                    <button class="lg:hidden p-2 border border-gray-500 rounded" @click="toggleMenu">
+                        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M4 6h16M4 12h16m-7 6h7"></path>
+                        </svg>
+                    </button>
+                </div>
+                <div :class="{
+                    hidden: !isMenuOpen,
+                    flex: isMenuOpen,
+                    'lg:flex': true,
+                    'lg:items-center': true,
+                    'lg:w-auto': true,
+                    'w-full': true
+                }" id="navbarSupportedContent">
+                    <div class="flex flex-col lg:flex-row lg:items-center lg:space-x-4 w-full lg:w-auto">
+                        <RouterLink to="/"
+                            class="font-medi px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">
+                            Home</RouterLink>
+                        <RouterLink to="/about"
+                            class="font-medi px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">
+                            About</RouterLink>
+                        <RouterLink to="/login"
+                            class="font-medi px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">
+                            Login</RouterLink>
+                    </div>
+                    <div class="me-6 mt-3 lg:mt-0 lg:ml-auto text-center lg:text-left">
+                        <span class="text-lg font-medium">Hi Manager</span>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <RouterView />
+</template>
+
+<style scoped></style>
+
+<script>
+export default {
+    data() {
+        return {
+            isMenuOpen: false,
+        };
+    },
+    methods: {
+        toggleMenu() {
+            this.isMenuOpen = !this.isMenuOpen;
+        },
+    },
+};
+</script>
---
+++ frontend/food-order-app/src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+<template>
+  <main class="mt-8">
+    <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+        <div class="md:flex">
+            <div class="md:shrink-0">
+                <img class="h-48 w-full object-cover md:h-full md:w-48" src="https://i.pinimg.com/736x/ff/2f/41/ff2f415288e3af5cef6bae9cb34d6089.jpg" alt="Modern building architecture">
+            </div>
+            <div class="p-8">
+                <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">girl with blue hair</div>
+                <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">hello kitten ! nya</a>
+                <p class="mt-2 text-slate-500">I see you're hungry. What can I cook up to satisfy your cravings?</p>
+            </div>
+        </div>
+    </div>
+</main>
+
+</template>
